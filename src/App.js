45b135f3a1b431af74ac60e2fd77d7db9badd4e6
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-12 23:03:30 +0000

    Clean the project the starting point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    //BEM class naming convention
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Let' build youtube</h1>
+
+        {/* Header <Header/> */}
+          
+        {/* Sidebar <Sidebar/> */}
+
+        {/* RecpmandedVideo */}
+        
+
     </div>
   );
 }
