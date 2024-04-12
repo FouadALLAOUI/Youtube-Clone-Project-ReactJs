a7baef1acde78d7ea86b69db5751b7a3f74182a5
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-12 23:14:38 +0000

    Matirial UI/Core + Matirial UI/iCons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     //BEM class naming convention
     <div className="App">
-      <h1>Hello Let' build youtube</h1>
+        {/*<h1>Hello Let' build youtube</h1>*/}
 
         {/* Header <Header/> */}
           <Header/> 
