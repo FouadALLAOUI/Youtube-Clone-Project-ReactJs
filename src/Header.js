064135008014af0fcb510d52667a2f835a6be81b
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-13 00:12:37 +0000

    Styling the header component

--- src/Header.js
+++ src/Header.js
@@ -5,28 +5,34 @@
  import AppsIcon from "@material-ui/icons/Apps";
  import NotificationsIcon from "@material-ui/icons/Notifications";
  import Avatar  from '@material-ui/core/Avatar';
+ import './Header.css'
 
 function Header() {
   return (
     <div className='header'>
-      <h1>Header</h1>
-      <MenuIcon/>
-      <img className='header_logo
-      ' src='https://upload.wikimedia.org/wikipedia/commons/th
-      umb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1920px-
-      Logo_of_YouTube_%282015-2017%29.svg.png' 
-      alt=''/>
-      <input type='text'/>
-      <SearchIcon/>
-      <VideoCallIcon/>
-      <AppsIcon/>
-      <NotificationsIcon/>
-      <Avatar
+      <div className='header_left'>
+        <MenuIcon/>
+        <img className='header_logo
+        ' src='https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png' 
+        alt=''/>
+      </div>
+      <div className='header_input'>
+       <input placeholder='Search' type='text'/>
+       <SearchIcon  className='header_inputButton'/>
+      </div>
+      <div className='header_icons'>
+      <VideoCallIcon className='header_icon'/>
+      <AppsIcon className='header_icon'/>
+      <NotificationsIcon className='header_icon'/>
+      <Avatar 
         alt='Fouad'
         src='https://media.licdn.com/dms/image/D4E03AQHk7x_B9pwFLA/
         profile-displayphoto-shrink_800_800/0/1712458541239?e=1718236800&v
         =beta&t=NG1rTvdDRtBmXZOcviNQGEXNxldTqEYxTBa6ckPFa5c'
       />
+      </div>
+
+
     </div>
   )
 }
