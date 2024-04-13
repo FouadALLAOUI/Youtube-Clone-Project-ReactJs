09c2aa0fc83afd8c12f1d1ecdb72f59eb51deae8
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-13 19:48:45 +0000

    finish styling the recommendedvideos components  and install Reactrouter

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,113 @@
 import React from 'react';
-import "./RecommendedVideos.css"
+import "./RecommendedVideos.css";
+import VideoCard from './VideoCard';
 
 function RecommendedVideos() {
   return (
     <div className='recommendedVideos'>
         <h2>Recommended</h2>
+
+        <div className='recommendedVideos_videos'>
+          <VideoCard
+            title="Exclusive: Tucker Carlson Interviews Vladimir Putin" 
+            views="19K View"
+            timestamp="2 months"
+            channelImage="https://yt3.ggpht.com/uh8l4I70AViAR_jYzxSDlzQZEIDsoStEJj20EZ_16O-OGuCIt5Yw-vykVBjL-qn4XOUJ181XBg=s88-c-k-c0x00ffffff-no-rj"
+            channel=" Tucker Carlson"
+            image="https://i.ytimg.com/vi/fOCWBhuDdDo/maxresdefault.jpg"
+          />
+          <VideoCard
+            title="Andrew Tate EXCLUSIVE INTERVIEW: Jail Life | BBC Reaction | Matrix | Religion | Future Plans" 
+            views="1,72M View"
+            timestamp="10 months"
+            channelImage="https://m.media-amazon.com/images/M/MV5BNDdiNjAxMGMtNmU1Mi00N2ViLThhMTYtMTMxMmRhYWQ5ZTJjXkEyXkFqcGdeQXVyMDY3OTcyOQ@@._V1_.jpg"
+            channel="PBD Podcast"
+            image="https://i.ytimg.com/vi/uGO_ioFCRK0/mqdefault.jpg"
+          />
+          <VideoCard
+            title="Tristan Tate EXCLUSIVE INTERVIEW - Jail | Brotherhood | Politics | Religion | Fashion" 
+            views="7,39M View"
+            timestamp="1 year"
+            channelImage="https://m.media-amazon.com/images/M/MV5BNDdiNjAxMGMtNmU1Mi00N2ViLThhMTYtMTMxMmRhYWQ5ZTJjXkEyXkFqcGdeQXVyMDY3OTcyOQ@@._V1_.jpg"
+            channel="PBD Podcast"
+            image="https://i.ytimg.com/vi/TxLJEzG1T38/maxresdefault.jpg"
+          />
+          <VideoCard
+            title="TMike Tyson Reacts To Dylan Mulvaney Being Endorsed By Bud Light | PBD Podcast" 
+            views="721K View"
+            timestamp="10 months"
+            channelImage="https://m.media-amazon.com/images/M/MV5BNDdiNjAxMGMtNmU1Mi00N2ViLThhMTYtMTMxMmRhYWQ5ZTJjXkEyXkFqcGdeQXVyMDY3OTcyOQ@@._V1_.jpg"
+            channel="PBD Podcast"
+            image="https://i.ytimg.com/vi/bNeyzh4njt0/maxresdefault.jpg"
+          />
+          <VideoCard
+            title="Khabib Nurmagomedov Opens Up - Possible Comeback | LGBTQ | Hardest Puncher | Fathers Influence" 
+            views="3M View"
+            timestamp="1 year"
+            channelImage="https://m.media-amazon.com/images/M/MV5BNDdiNjAxMGMtNmU1Mi00N2ViLThhMTYtMTMxMmRhYWQ5ZTJjXkEyXkFqcGdeQXVyMDY3OTcyOQ@@._V1_.jpg"
+            channel="PBD Podcast"
+            image="https://i.ytimg.com/vi/01BBVAZ3HDY/maxresdefault.jpg"
+          />
+          <VideoCard
+            title="Joe Rogan Experience #1470 - Elon Musk" 
+            views="33M View"
+            timestamp="3 year"
+            channelImage="https://yt3.ggpht.com/ytc/AIdro_lDDEXdttwn0jgtnH_jqQs-FLTsVh57e_yq_3Q8J3fsA98=s88-c-k-c0x00ffffff-no-rj"
+            channel="PowerfulJRE"
+            image="https://www.teslarati.com/wp-content/uploads/2020/05/elon-musk-joe-rogan-1024x587.jpg"
+          />
+          <VideoCard
+            title="The Muslim Money Experts Ep.1: Khabib Nurmagomedov" 
+            views="1M View"
+            timestamp="1 year"
+            channelImage="https://yt3.ggpht.com/4NiXjHKlVAGBgfz-jMk09S3-vFCvJ2DwQ9xm0H7dagAl5i5ck0OtgM-ev5PuqE1VSu1-B_Sskg=s88-c-k-c0x00ffffff-no-rj"
+            channel="Wahed"
+            image="https://i.ytimg.com/vi/wDU0JknYff8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDxX7nPnmnW4RdqtT7V-PfVx9eGfA"
+          />
+          <VideoCard
+            title="The Muslim Money Experts Ep.2 (special release): Islam Makhachev " 
+            views="232K View"
+            timestamp="1 year"
+            channelImage="https://yt3.ggpht.com/4NiXjHKlVAGBgfz-jMk09S3-vFCvJ2DwQ9xm0H7dagAl5i5ck0OtgM-ev5PuqE1VSu1-B_Sskg=s88-c-k-c0x00ffffff-no-rj"
+            channel="Wahed"
+            image="https://assets-global.website-files.com/62d95cab901208903052c89b/654c6f623dffee07f76f2c43_64f07ade4c923c0e750da867_TMMG-EP2-Islam-YouTube.jpeg"
+          />
+          <VideoCard
+            title="Hasbulla USA Tour with Nelk Boys | Hotboxin’ with Mike Tyson" 
+            views="1M View"
+            timestamp="1 year"
+            channelImage="https://yt3.ggpht.com/ytc/AIdro_m2W6XWC0NJLVPxey9ex1CYIlU-s1qxX06CZik56b_C3g=s88-c-k-c0x00ffffff-no-rj"
+            channel="W Hotboxin' with Mike Tyson"
+            image="https://i.ytimg.com/vi/EjcmQSugrWU/maxresdefault.jpg"
+          />
+          <VideoCard
+            title="Khabib Nurmagomedov FINALLY Reveals His Secret To Success [EYE-OPENING]" 
+            views="415K View"
+            timestamp="1 year"
+            channelImage="https://yt3.ggpht.com/6QRTnhZxcSVO2dp1Tk02Vc_CZ0LJX8nRzTy5ptLHSm9UB0QYBY5OCAJjnXvdN64hZlSCTa8fpA=s88-c-k-c0x00ffffff-no-rj"
+            channel="Motivation Mentors"
+            image="https://i.ytimg.com/vi/ibvWWlbcsEI/maxresdefault.jpg"
+          />
+          <VideoCard
+            title="The Muslim Money Experts Ep.3: Paul Pogba" 
+            views="132K View"
+            timestamp="1 year"
+            channelImage="https://yt3.ggpht.com/4NiXjHKlVAGBgfz-jMk09S3-vFCvJ2DwQ9xm0H7dagAl5i5ck0OtgM-ev5PuqE1VSu1-B_Sskg=s88-c-k-c0x00ffffff-no-rj"
+            channel="Wahed"
+            image="https://i.ytimg.com/vi/5ZsnDE9JfVs/maxresdefault.jpg"
+          />
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+          <VideoCard/>
+        </div>
+
+
+
     </div>
   )
 }
