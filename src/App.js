cdef4fa9583ea789549c84c1ad1e7a9743769613
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-14 12:53:20 +0100

    Declare Router

--- src/App.js
+++ src/App.js
@@ -2,21 +2,32 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import RecommendedVideos from './RecommendedVideos';
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
     //BEM class naming convention
     <div className="App">
-        {/*<h1>Hello Let' build youtube</h1>*/}
+          {/*<h1>Hello Let' build youtube</h1>*/}
+          {/** 
+          <Router>
+              <Switch>
+                <Route path='/'>
+                  <h1>Home page</h1>
+                </Route>
+              </Switch>
+          </Router>
+          */}
 
-        {/* Header <Header/> */}
+          {/* Header <Header/> */}
           <Header/> 
           <div className='app_page'>
             {/* Sidebar <Sidebar/> */}
             <Sidebar />          
             {/* RecommendedVideos */}
             <RecommendedVideos/>
-          </div>      
+          </div> 
 
     </div>
   );
